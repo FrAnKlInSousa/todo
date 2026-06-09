ae58fa8dac1591a5d9d86bfdc6181cb70a5c4793
franklin <frank.fkn.23@gmail.com>
2026-06-09 11:40:07 -0300

    schemas userPublic, UserSchema e UserDB

---
+++ todo/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserPublic(BaseModel):
+    username: str
+    email: EmailStr
+    id: int
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserDB(UserSchema):
+    id: int
