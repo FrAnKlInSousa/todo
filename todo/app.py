b91f6aff1e347ff6e9ab418b1ec0c42b1fe24482
franklin <frank.fkn.23@gmail.com>
2026-06-09 11:39:30 -0300

    endpoint create user

--- todo/app.py
+++ todo/app.py
@@ -1,8 +1,21 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
+from todo.schemas import UserDB, UserPublic, UserSchema
+
 app = FastAPI()
+
+database = []
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return user_with_id
