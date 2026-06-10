0ae4c751602123292f79ee25b04f1c745162f99c
franklin <frank.fkn.23@gmail.com>
2026-06-10 17:38:34 -0300

    configuração de variável de ambiente no settings.py

---
+++ todo/settings.py
@@ -0,0 +1,8 @@
+from pydantic_settings import SettingsConfigDict, BaseSettings
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env', env_file_encoding='utf-8'
+    )
+
+    DATABASE_URL: str
