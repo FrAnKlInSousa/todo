0d3913158e794a9a84d301839a718ae7a838dfaf
franklin <frank.fkn.23@gmail.com>
2026-06-12 17:50:26 -0300

    integrando o banco de dados sqlite e aplicando o uso do Depends

--- todo/app.py
+++ todo/app.py
@@ -1,8 +1,13 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
 
-from todo.schemas import UserDB, UserList, UserPublic, UserSchema
+from todo.database import get_session
+from todo.models import User
+from todo.schemas import Message, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -15,37 +20,76 @@ def read_root():
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
-    database.append(user_with_id)
-    return user_with_id
+def create_user(user: UserSchema, session=Depends(get_session)):
+
+    user_db = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
+    # todo verificar caso passe ana@mail (sem o .com), pois não estava dando
+    #  erro no test_update_integrity_error
+
+    if user_db:
+        raise HTTPException(
+            detail='Email or username already exists.',
+            status_code=HTTPStatus.CONFLICT,
+        )
+
+    user_db = User(**user.model_dump())
+
+    session.add(user_db)
+
+    session.commit()
+    session.refresh(user_db)
+    return user_db
 
 
 @app.get('/users/', response_model=UserList)
-def read_users():
-    return {'users': database}
+def read_users(
+    limit: int = 10, offset: int = 0, session: Session = Depends(get_session)
+):
+    users = session.scalars(select(User).limit(limit).offset(offset))
+    return {'users': users}
 
 
 @app.put(
     '/users/{user_id}', response_model=UserPublic, status_code=HTTPStatus.OK
 )
-def update_user(user_id: int, user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    if user_id < 1 or user_id > len(database):
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
+    user_db = session.scalar(select(User).where(User.id == user_id))
+    if not user_db:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='Usuário não encontrado.'
+            detail='User not found', status_code=HTTPStatus.NOT_FOUND
         )
+    try:
+        user_db.email = user.email
+        user_db.username = user.username
+        user_db.password = user.password
 
-    database[user_id - 1] = user_with_id
-    return user_with_id
+        session.add(user_db)
+        session.commit()
+        session.refresh(user_db)
+        return user_db
+    except IntegrityError:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT,
+            detail='Username or email already exists',
+        )
 
 
 @app.delete(
-    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=Message
 )
-def delete_user(user_id: int):
-    if user_id < 1 or user_id > len(database):
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    user_db = session.scalar(select(User).where(User.id == user_id))
+
+    if not user_db:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='Usuário não encontrado.'
+            detail='User not found', status_code=HTTPStatus.NOT_FOUND
         )
-    return database.pop(user_id - 1)
+    session.delete(user_db)
+    session.commit()
+    return {'message': 'User deleted'}
