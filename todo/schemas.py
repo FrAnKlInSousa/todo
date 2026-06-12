3cd13be478c17a8ad1299c9b11494d07e7e9dad1
franklin <frank.fkn.23@gmail.com>
2026-06-12 17:49:42 -0300

    configurando o configDict do pydantic para lidar com o ORM do sqlalchemy

--- todo/schemas.py
+++ todo/schemas.py
@@ -1,10 +1,15 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
+
+
+class Message(BaseModel):
+    message: str
 
 
 class UserPublic(BaseModel):
     username: str
     email: EmailStr
     id: int
+    model_config = ConfigDict(from_attributes=True)
 
 
 class UserSchema(BaseModel):
