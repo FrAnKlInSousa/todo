5dd3f2e09ee420b9d493e11952ad37575021996e
franklin <frank.fkn.23@gmail.com>
2026-06-13 11:25:33 -0300

    criando função para gerar hash da senha

---
+++ todo/security.py
@@ -0,0 +1,11 @@
+from pwdlib import PasswordHash
+
+
+pwd_context = PasswordHash.recommended()
+
+def create_password_hash(password: str):
+    pwd_context.hash(password=password)
+
+
+def verify_password(password: str, hashed_password: str):
+    return pwd_context.verify(password=password, hash=hashed_password)
