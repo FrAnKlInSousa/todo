fd0b5e97eb76355d33f07ee0b74ed1b90a68b0ec
franklin <frank.fkn.23@gmail.com>
2026-06-13 20:17:02 -0300

    criando o current_user e o create_access_token

--- todo/security.py
+++ todo/security.py
@@ -1,6 +1,22 @@
+from datetime import datetime, timedelta
+from http import HTTPStatus
+from zoneinfo import ZoneInfo
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jwt import DecodeError, decode, encode
 from pwdlib import PasswordHash
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from todo.database import get_session
+from todo.models import User
 
 pwd_context = PasswordHash.recommended()
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = 'YOU_SECRET_KEY'
+ALGORITHM = 'HS256'
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 
 def create_password_hash(password: str):
@@ -9,3 +25,39 @@ def create_password_hash(password: str):
 
 def verify_password(password: str, hashed_password: str):
     return pwd_context.verify(password=password, hash=hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({'exp': expire})
+
+    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_user(
+    session: Session = Depends(get_session),
+    token: str = Depends(oauth2_scheme),
+):
+    credentials_exception = HTTPException(
+        detail='Could not validate credentials',
+        status_code=HTTPStatus.UNAUTHORIZED,
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+    try:
+        payload = decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        subject_email = payload.get('sub')
+        if not subject_email:
+            raise credentials_exception
+    except DecodeError:
+        raise credentials_exception
+
+    user = session.scalar(select(User).where(subject_email == User.email))
+    if not user:
+        raise credentials_exception
+    return user
