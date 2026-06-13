ea13d2542da60061d3a1232aa2af24fd4fc56f94
franklin <frank.fkn.23@gmail.com>
2026-06-13 12:30:28 -0300

    alterando o nome da variavel de contexto

--- todo/security.py
+++ todo/security.py
@@ -1,10 +1,10 @@
 from pwdlib import PasswordHash
 
-
 pwd_context = PasswordHash.recommended()
 
+
 def create_password_hash(password: str):
-    pwd_context.hash(password=password)
+    return pwd_context.hash(password=password)
 
 
 def verify_password(password: str, hashed_password: str):
