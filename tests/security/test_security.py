579a38715578afb1844538bfa7d2fe6acddb13a8
franklin <frank.fkn.23@gmail.com>
2026-06-13 21:15:17 -0300

    cobrindo cenários de teste get_current_user

--- tests/security/test_security.py
+++ tests/security/test_security.py
@@ -28,3 +28,27 @@ def test_jwt_invalid_token(client, user):
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_jwt_with_no_subject(client):
+    token = create_access_token({'no-email': 'test'})
+    response = client.delete(
+        '/users/1',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_jwt_with_no_user(client):
+    data = {'sub': 'not_found@mail.com'}
+
+    token = create_access_token(data)
+    response = client.delete(
+        '/users/1', headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+    assert response.json() == {'detail': 'Could not validate credentials'}
