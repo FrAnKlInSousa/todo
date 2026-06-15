bcf57092691619c12df3eb71e9ffb4563e704e7f
franklin <frank.fkn.23@gmail.com>
2026-06-15 12:33:44 -0300

    adicionando teste de remoção de usuário por outro usuário

--- tests/user/test_delete_user.py
+++ tests/user/test_delete_user.py
@@ -17,3 +17,16 @@ def test_delete_user_not_found(client, token):
 
     assert response.status_code == HTTPStatus.FORBIDDEN
     assert response.json() == {'detail': 'Not enough permissions'}
+
+
+def test_delete_user_with_wrong_user(client, other_user, token):
+    response = client.delete(
+        f'/users/{other_user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {'detail': 'Not enough permissions'}
+
+
+# delete
