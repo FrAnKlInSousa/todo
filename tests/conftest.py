b651ef8b1543e4f13c6cf268722233a0f63b902d
franklin <frank.fkn.23@gmail.com>
2026-06-15 11:53:36 -0300

    criando user_factory e o other_user

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 from datetime import datetime
 
+import factory
 import pytest
 import pytest_asyncio
 from fastapi.testclient import TestClient
@@ -63,11 +64,18 @@ def mock_db_time():
 @pytest_asyncio.fixture
 async def user(session: AsyncSession):
     password = 'secret123'
-    user = User(
-        username='user_test',
-        email='test@mail.com',
-        password=create_password_hash(password),
-    )
+    user = UserFactory(password=create_password_hash(password))
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    user.clean_password = password
+    return user
+
+
+@pytest_asyncio.fixture
+async def other_user(session: AsyncSession):
+    password = 'secret123'
+    user = UserFactory(password=create_password_hash(password))
     session.add(user)
     await session.commit()
     await session.refresh(user)
@@ -90,3 +98,12 @@ def settings():
     # exemplo de manipulação que dá pra fazer com essa fixture
     settings.ACCESS_TOKEN_EXPIRE_MINUTES = 35
     return settings
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f'test{n}')
+    email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
+    password = factory.LazyAttribute(lambda obj: f'{obj.username}_secret')
