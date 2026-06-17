cfe6c27791e44db6d6ca2fbbcc5ebbeb474a9083
franklin <frank.fkn.23@gmail.com>
2026-06-17 13:12:50 -0300

    criando rota para criar todo e adicionando ela na aplicação

--- todo/app.py
+++ todo/app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
-from todo.routers import auth, users
+from todo.routers import auth, todos, users
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(todos.router)
 
 
 @app.get('/')
--- todo/routers/todos.py
+++ todo/routers/todos.py
@@ -1,5 +1,25 @@
-from fastapi import APIRouter
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from todo.database import get_session
+from todo.models import Todo, User
+from todo.schemas import TodoPublic, TodoSchema
+from todo.security import get_current_user
 
 router = APIRouter(prefix='/todos', tags=['todos'])
 
-@router.post('/')
+Session = Annotated[AsyncSession, Depends(get_session)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
+
+@router.post('/', response_model=TodoPublic, status_code=HTTPStatus.CREATED)
+async def create_todo(todo: TodoSchema, session: Session, user: CurrentUser):
+    db_todo = Todo(**todo.model_dump(), user_id=user.id)
+
+    session.add(db_todo)
+    await session.commit()
+    await session.refresh(db_todo)
+    return db_todo
