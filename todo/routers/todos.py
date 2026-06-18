25e8636cd8c6edf30907318263a7b423e8046dd9
franklin <frank.fkn.23@gmail.com>
2026-06-17 21:28:49 -0300

    rota de listagem de todos

--- todo/routers/todos.py
+++ todo/routers/todos.py
@@ -1,12 +1,13 @@
 from http import HTTPStatus
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from todo.database import get_session
 from todo.models import Todo, User
-from todo.schemas import TodoPublic, TodoSchema
+from todo.schemas import FilterTodo, TodoList, TodoPublic, TodoSchema
 from todo.security import get_current_user
 
 router = APIRouter(prefix='/todos', tags=['todos'])
@@ -23,3 +24,27 @@ async def create_todo(todo: TodoSchema, session: Session, user: CurrentUser):
     await session.commit()
     await session.refresh(db_todo)
     return db_todo
+
+
+@router.get('/', response_model=TodoList, status_code=HTTPStatus.OK)
+async def list_todos(
+    user: CurrentUser,
+    session: Session,
+    todo_filter: Annotated[FilterTodo, Query()],
+):
+    query = select(Todo).where(Todo.user_id == user.id)
+
+    if todo_filter.state:
+        query.filter(Todo.state == todo_filter.state)
+
+    if todo_filter.title:
+        query.filter(Todo.title == todo_filter.title)
+
+    if desc := todo_filter.description:  # exemplo com operador walrus
+        query.filter(Todo.description == desc)
+
+    todos = await session.scalars(
+        query.limit(todo_filter.limit).offset(todo_filter.offset)
+    )
+
+    return {'todos': todos.all()}
