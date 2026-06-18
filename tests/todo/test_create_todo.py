9ca9555786ef86b1eeefc44275efef0e6dfdbb45
franklin <frank.fkn.23@gmail.com>
2026-06-17 23:08:38 -0300

    valida criação dos todos

--- tests/todo/test_create_todo.py
+++ tests/todo/test_create_todo.py
@@ -1,5 +1,21 @@
 from http import HTTPStatus
 
+import factory.fuzzy
+import pytest
+
+from todo.models import Todo
+from todo.schemas import TodoState
+
+
+class TodoFactory(factory.Factory):
+    class Meta:
+        model = Todo
+
+    title = factory.Faker('text')
+    description = factory.Faker('text')
+    user_id = 1
+    state = factory.fuzzy.FuzzyChoice(TodoState)
+
 
 def test_create_todo(client, token):
     response = client.post(
@@ -21,3 +37,18 @@ def test_create_todo(client, token):
         'id': 1,
         'user_id': 1,
     }
+
+
+@pytest.mark.asyncio
+async def test_should_create_5_todos(client, session, token):
+
+    expected_todos = 5
+    session.add_all(TodoFactory.create_batch(5))
+    await session.commit()
+
+    response = client.get(
+        '/todos', headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert len(response.json()['todos']) == expected_todos
