004acd57926176bb7e3cefd6476fa23248426529
franklin <frank.fkn.23@gmail.com>
2026-06-20 19:12:36 -0300

    criando fixture engine com scopo de session para o testecontainer

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,25 +27,30 @@ def client(session):
     app.dependency_overrides.clear()
 
 
-@pytest_asyncio.fixture
-async def session():
+@pytest.fixture(scope='session')
+def engine():
     with PostgresContainer('postgres:17', driver='psycopg') as postgres:
-        engine = create_async_engine(
+        yield create_async_engine(
             postgres.get_connection_url(),
         )
-        # engine = create_async_engine(
-        #     'sqlite+aiosqlite:///:memory:',
-        #     connect_args={'check_same_thread': False},
-        #     poolclass=StaticPool,
-        # )
-        async with engine.begin() as conn:
-            await conn.run_sync(table_registry.metadata.create_all)
 
-        async with AsyncSession(engine, expire_on_commit=False) as session:
-            yield session
 
-        async with engine.begin() as conn:
-            await conn.run_sync(table_registry.metadata.drop_all)
+@pytest_asyncio.fixture
+async def session(engine):
+
+    # engine = create_async_engine(
+    #     'sqlite+aiosqlite:///:memory:',
+    #     connect_args={'check_same_thread': False},
+    #     poolclass=StaticPool,
+    # )
+    async with engine.begin() as conn:
+        await conn.run_sync(table_registry.metadata.create_all)
+
+    async with AsyncSession(engine, expire_on_commit=False) as session:
+        yield session
+
+    async with engine.begin() as conn:
+        await conn.run_sync(table_registry.metadata.drop_all)
 
 
 @contextmanager
