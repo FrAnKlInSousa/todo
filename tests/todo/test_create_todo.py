49a8007d29c9a32ed0be110ca5aa92a5d7b0c0a0
franklin <frank.fkn.23@gmail.com>
2026-06-20 19:14:41 -0300

    ajustando teste com pytest.raises

--- tests/todo/test_create_todo.py
+++ tests/todo/test_create_todo.py
@@ -1,9 +1,11 @@
 from http import HTTPStatus
 
 import pytest
+from sqlalchemy import select
+from sqlalchemy.exc import DataError, PendingRollbackError
 
 from tests.factories.todo_factory import TodoFactory
-from todo.models import Todo
+from todo.models import Todo, User
 
 
 def test_create_todo(client, token, mock_db_time):
@@ -44,3 +46,39 @@ async def test_should_create_5_todos(client, session, token):
 
     assert response.status_code == HTTPStatus.OK
     assert len(response.json()['todos']) == expected_todos
+
+
+@pytest.mark.asyncio
+async def test_create_todo_error(session, user: User):
+    todo = Todo(
+        title='Test Todo',
+        description='Test Desc',
+        state='test',
+        user_id=user.id,
+    )
+
+    session.add(todo)
+
+    with pytest.raises(DataError):
+        await session.commit()
+
+    with pytest.raises(PendingRollbackError):
+        await session.scalar(select(Todo))
+
+
+def test_should_rise_error_on_todo_create_with_invalid_state(client, token):
+    response = client.post(
+        '/todos',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'title': 'Test Todo',
+            'description': 'Test Desc',
+            'state': 'test',
+        },
+    )
+    response_message = response.json()['detail'][0]['msg']
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_CONTENT
+    assert (
+        response_message
+        == "Input should be 'draft', 'todo', 'doing', 'done' or 'trash'"
+    )
