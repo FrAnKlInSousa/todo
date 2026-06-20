a0460ac27700597aa03f35cd0a5e1d7e4490edce
franklin <frank.fkn.23@gmail.com>
2026-06-20 19:00:20 -0300

    configurando a sessão com o testcontainers

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ import pytest_asyncio
 from fastapi.testclient import TestClient
 from sqlalchemy import event
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from testcontainers.postgres import PostgresContainer
 
 from tests.factories.user_factory import UserFactory
 from todo.app import app
@@ -28,22 +29,23 @@ def client(session):
 
 @pytest_asyncio.fixture
 async def session():
-    engine = create_async_engine(
-        Settings().DATABASE_URL,
-    )
-    # engine = create_async_engine(
-    #     'sqlite+aiosqlite:///:memory:',
-    #     connect_args={'check_same_thread': False},
-    #     poolclass=StaticPool,
-    # )
-    async with engine.begin() as conn:
-        await conn.run_sync(table_registry.metadata.create_all)
+    with PostgresContainer('postgres:17', driver='psycopg') as postgres:
+        engine = create_async_engine(
+            postgres.get_connection_url(),
+        )
+        # engine = create_async_engine(
+        #     'sqlite+aiosqlite:///:memory:',
+        #     connect_args={'check_same_thread': False},
+        #     poolclass=StaticPool,
+        # )
+        async with engine.begin() as conn:
+            await conn.run_sync(table_registry.metadata.create_all)
 
-    async with AsyncSession(engine, expire_on_commit=False) as session:
-        yield session
+        async with AsyncSession(engine, expire_on_commit=False) as session:
+            yield session
 
-    async with engine.begin() as conn:
-        await conn.run_sync(table_registry.metadata.drop_all)
+        async with engine.begin() as conn:
+            await conn.run_sync(table_registry.metadata.drop_all)
 
 
 @contextmanager
